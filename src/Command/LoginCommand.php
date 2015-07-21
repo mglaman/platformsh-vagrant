a3475f43b919f8c1abc1df281a8d1cb1b4e189a6
Matt Glaman <nmd.matt@gmail.com>
2015-07-21 11:34:38 -0500

    Allow running "login" on environment

--- src/Command/LoginCommand.php
+++ src/Command/LoginCommand.php
@@ -2,8 +2,8 @@
 
 namespace Platformsh\Vagrant\Command;
 
-use Platformsh\Vagrant\Config\ProjectConfig;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
 
@@ -21,7 +21,8 @@ class LoginCommand extends Command
   {
       $this
         ->setName('login')
-        ->setDescription('Logs into the Drupal project');
+        ->setDescription('Logs into the Drupal project')
+        ->addOption('environment', null, InputOption::VALUE_NONE, 'Run command on Platform.sh environment');
   }
 
   /**
@@ -37,9 +38,17 @@ class LoginCommand extends Command
    */
   protected function execute(InputInterface $input, OutputInterface $output)
   {
-    $projectName = $this->getProjectConfig('project_name');
-    // @todo: get proper hostname from Vagrant config.
-    $process = new Process("vagrant -c ssh -c '/usr/local/bin/drush uli --root=/var/www/platformsh/www --uri=$projectName.platformsh.dev' ");
+    // If --environment, run on Platform.sh environment. Otherwise the VM.
+    if ($input->getOption('environment')) {
+      $projectId = $this->getProjectConfig('project_id');
+      $projectEnv = $this->getProjectConfig('project_environment');
+      $process = new Process("drush @$projectId.$projectEnv uli");
+
+    } else {
+      $projectName = $this->getProjectConfig('project_name');
+      // @todo: get proper hostname from Vagrant config.
+      $process = new Process("vagrant -c ssh -c '/usr/local/bin/drush uli --root=/var/www/platformsh/www --uri=$projectName.platformsh.dev' ");
+    }
     $this->runProcess($process);
   }
 }
